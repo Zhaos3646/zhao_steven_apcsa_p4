72ddbe32f739067738bbab5291ae62ff5c6b2e45
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-02-05 14:13:40 -0800

    Adding Unit 2

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,14 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		float base = base1+base2;
+		return 0.5*base*height;
+	}
+}

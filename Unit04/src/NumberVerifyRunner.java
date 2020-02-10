d803e63750c1fa8ba325a909caf856dc7352cc5d
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-02-10 13:31:51 -0800

    Added to NumberVerifyRunner

--- Unit04/src/NumberVerifyRunner.java
+++ Unit04/src/NumberVerifyRunner.java
@@ -18,5 +18,9 @@ public class NumberVerifyRunner
 		System.out.println("5 is even :: " + NumberVerify.isEven(5));
 		
 		//add in more test cases
+		System.out.println("7 is even:: " + NumberVerify.isEven(7));
+		System.out.println("3 is odd:: " + NumberVerify.isOdd(3));
+		System.out.println("8 is even:: " + NumberVerify.isEven(8));
+		System.out.println("8 is odd:: " + NumberVerify.isOdd(8));
 	}
 }

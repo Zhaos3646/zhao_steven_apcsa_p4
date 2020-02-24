098e2d4820c0c852e66439ee72d74c1d9feba0e6
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-02-24 13:37:06 -0800

    Debugged RayOddToEven

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,22 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int cnt = 0;
+		boolean set = false;
+		for (int i = 0; i < ray.length; i++) {
+			if (ray[i] % 2 == 1 && set == false) {
+				set = true;
+			}
+			else if (ray[i] % 2 == 0 && set == true) {
+				set = false;
+				return cnt;
+			}
+			if (set == true) {
+				cnt ++;
+			}
+		}
+		return -1;
 	}
 }

f85e2c114e15bc8893071f534843e5988fdff82d
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-03-10 14:53:05 -0700

    Uploading Unit 9

--- Unit09/src/DownRunner.java
+++ Unit09/src/DownRunner.java
@@ -36,16 +36,16 @@ public class DownRunner
 		test.addAll(Arrays.asList(-111,1,2,3,9,11,20,30));
 		System.out.println(ListDown.go(test));
 		test.clear();
-		test.addAll(Arrays.asList(32767));
+		test.addAll(Arrays.asList(9,8,7,6,5,4,3,2,0,-2,-989));
 		System.out.println(ListDown.go(test));
 		test.clear();
-		test.addAll(Arrays.asList(32767));
+		test.addAll(Arrays.asList(12,15,18,21,23,1000));
 		System.out.println(ListDown.go(test));
 		test.clear();
-		test.addAll(Arrays.asList(32767));
+		test.addAll(Arrays.asList(250,19,17,15,13,11,10,9,6,3,2,1,-455));
 		System.out.println(ListDown.go(test));
 		test.clear();
-		test.addAll(Arrays.asList(32767));
+		test.addAll(Arrays.asList(9,10,-8,10000,-5000,1000));
 		System.out.println(ListDown.go(test));
 	}
 }

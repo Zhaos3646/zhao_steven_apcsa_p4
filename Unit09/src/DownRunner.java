f5bd0682a734a075127f5d9582abd29a2e1f3478
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-03-10 14:43:30 -0700

    Uploading Activity5

--- Unit09/src/DownRunner.java
+++ Unit09/src/DownRunner.java
@@ -3,9 +3,49 @@
 //Name -
 //Date -
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{
+		List test = new ArrayList<Integer>();
+		test.addAll(Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,12345));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(10,9,8,7,6,5,4,3,2,1,-99));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(10,20,30,40,50,-11818,40,30,20,10));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(32767));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(255,255));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(9,10,-88,100,-555,1000));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(10,10,10,11,456));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(-111,1,2,3,9,11,20,30));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(32767));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(32767));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(32767));
+		System.out.println(ListDown.go(test));
+		test.clear();
+		test.addAll(Arrays.asList(32767));
+		System.out.println(ListDown.go(test));
 	}
 }
--- Unit09/src/ListOddToEven.java
+++ Unit09/src/ListOddToEven.java
@@ -8,8 +8,25 @@ import java.util.List;
 
 public class ListOddToEven
 {
-	public static boolean go( List<Integer> ray )
+	public static int go( List<Integer> ray )
 	{
-		return false;
+		int count = 0;
+		int k = 0;
+		boolean check = false;
+		for (int i = 0; i < ray.size(); i ++) {
+			if (ray.get(i) % 2 == 1 && check == false) {
+				check = true;
+			}
+			if (check == true && ray.get(i) % 2 == 0) {
+				count = k;
+			}
+			if (check == true) {
+				k ++;
+			}
+		}
+		if (count == 0) {
+			return -1;
+		}
+		return count;
 	}
 }

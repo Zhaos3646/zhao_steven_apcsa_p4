1c34976c283d62ce504a710f26455245ef6f33a0
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2020-04-24 23:27:22 -0700

    Updating Unit13

--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -12,15 +12,55 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		String word;
+		int count = 0;
+		Scanner sent = new Scanner(sentence);
+		while(sent.hasNext()) {
+			count ++;
+		}
+		wordRay = new String[count];
+		for (int i = 0; i < count; i ++) {
+			wordRay[i] = sent.next();
+		}
+		wordRay = sort(wordRay);
+		
 	}
 
-	public void sort()
+	public String[] sort(String[] array)
 	{
+		for(int i=0; i< array.length-1; i++){
+
+			int min = i;
+
+			for(int j = i+1; j< array.length; j++)
+
+			{
+
+			if( array[j].charAt(0) < array[j].charAt(0))
+
+			min = j;
+			//find location of smallest
+
+			}
+
+			if(min != i) {
+
+			String temp = array[min];
+
+			array[min] = array[i];
+
+			array[i] = temp;
+	}
+	}
+		return array;
 	}
 
 	public String toString()
 	{
 		String output="";
+		for (int i = 0; i < wordRay.length; i ++) {
+			output = output + wordRay[i] + "\n";
+		}
 		return output+"\n\n";
 	}
 }
